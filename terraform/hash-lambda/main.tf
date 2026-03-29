402e3fcf0003bc8b14d93e4fc8e3a2ad336d11a5
Svetlozar Kapitanov <svetlio04@abv.bg>
2026-03-29 20:31:00 +0300

    KAN-6 lambda + terraform improvements

--- terraform/hash-lambda/main.tf
+++ terraform/hash-lambda/main.tf
@@ -1,18 +1,45 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
     }
   }
 }
 
-provider "aws" {
-  region = "eu-central-1"
+variable "prefix" {
+  type = string
 }
 
+variable "table_name" {
+  type = string
+}
+
+variable "hash_length" {
+  type    = number
+  default = 6
+}
+
+variable "max_hash_attempts" {
+  type    = number
+  default = 10
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+#zipvane
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda.js"
+  output_path = "${path.module}/lambda.zip"
+}
+
+#IAM Role
 resource "aws_iam_role" "lambda_role" {
-  name = "hash-lambda-role"
+  name = "${var.prefix}-hash-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -25,46 +52,55 @@ resource "aws_iam_role" "lambda_role" {
     }]
   })
 }
-
+#basic logging
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-resource "aws_iam_role_policy" "dynamodb_access" {
-  name = "hash-lambda-dynamodb-policy"
+#least privilige
+resource "aws_iam_role_policy" "dynamodb_policy" {
+  name = "${var.prefix}-dynamodb-policy"
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem"
-        ]
-        Resource = "*"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ]
+      Resource = "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.table_name}"
+    }]
   })
 }
 
 resource "aws_lambda_function" "hash_lambda" {
-  function_name = "hash-lambda"
+  function_name = "${var.prefix}-hash-lambda"
   role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "lambda.handler"
 
-  filename         = "lambda.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
-  timeout          = 10
+  runtime = "nodejs24.x"
+  handler = "lambda.handler"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  timeout     = 10
+  memory_size = 128
 
   environment {
     variables = {
-      TABLE_NAME        = "UrlMappings"
-      HASH_LENGTH       = "6"
-      MAX_HASH_ATTEMPTS = "10"
+      TABLE_NAME        = var.table_name
+      HASH_LENGTH       = tostring(var.hash_length)
+      MAX_HASH_ATTEMPTS = tostring(var.max_hash_attempts)
     }
   }
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.hash_lambda.function_name
+}
+
+output "lambda_arn" {
+  value = aws_lambda_function.hash_lambda.arn
 }
