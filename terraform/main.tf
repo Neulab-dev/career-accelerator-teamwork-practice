0c6385b1b668c93b719b111928e6fb68ad4b698e
Svetlozar Kapitanov <svetlio04@abv.bg>
2026-03-29 21:37:39 +0300

    removed DynamoDB table resource to avoid conflict with KAN-7

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,26 +21,13 @@ variable "table_name" {
   default = "shortly-urls"
 }
 
-#DynamoDB
-resource "aws_dynamodb_table" "urls" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "hash"
-
-  attribute {
-    name = "hash"
-    type = "S"
-  }
-}
-
-#Lambda modul
 module "hash_lambda" {
   source = "./hash-lambda"
 
-  prefix              = var.prefix
-  table_name          = aws_dynamodb_table.urls.name
-  hash_length         = 6
-  max_hash_attempts   = 10
+  prefix            = var.prefix
+  table_name        = var.table_name
+  hash_length       = 6
+  max_hash_attempts = 10
 }
 
 output "lambda_name" {
@@ -49,8 +36,4 @@ output "lambda_name" {
 
 output "lambda_arn" {
   value = module.hash_lambda.lambda_arn
-}
-
-output "dynamodb_table" {
-  value = aws_dynamodb_table.urls.name
 }
