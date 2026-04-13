a1bde2af9a045b846c448e84d5de27654b804472
SvetlozarzzzKapitanov <148339783+SvetlozarzzzKapitanov@users.noreply.github.com>
2026-04-13 18:29:17 +0300

    Kan 6 (#3)
    * Add hash lambda terraform setup
    
    * KAN-6 lambda + terraform improvements
    
    * updated deprecated attribute .name to .region in main.tf
    
    * removed DynamoDB table resource to avoid conflict with KAN-7
    
    * Address PR feedback:
    - Moved variables and outputs to separate files
    - Cleaned up module structure
    - Fixed IAM policy to use DynamoDB ARN
    - Removed unused resources
    - Fixed env variable naming
    
    * Clean up Terraform + Lambda implementation
    
    * Moved DynamoDB table to root module and passed ARN/name to Lambda module
    * Updated IAM policy to use injected DynamoDB ARN instead of direct resource reference
    * Removed unused outputs and deleted unnecessary outputs.tf files
    * Clarified provider usage (kept required_providers, removed provider config from module)
    * Fixed Node.js import to use `node:crypto`
    * Replaced Math.random() with crypto-secure randomness for hash generation
    * Minor cleanup and consistency improvements
    
    * Remove providers from module

---
+++ terraform/hash-lambda/variables.tf
@@ -0,0 +1,20 @@
+variable "prefix" {
+  type = string
+}
+
+variable "table_name" {
+  type = string
+}
+
+variable "hash_length" {
+  type    = number
+  default = 6
+}
+
+variable "max_hash_attempts" {
+  type    = number
+  default = 10
+}
+variable "table_arn" {
+  type = string
+}
