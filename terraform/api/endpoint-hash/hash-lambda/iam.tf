06321135d2774a85b40a352be606fbfe916e4e16
SinestroWhite <g.yokov@hotmail.com>
2026-05-11 18:45:20 +0300

    Update Lambda IAM policies for VPC access and disable unused EC2 policy

--- terraform/api/endpoint-hash/hash-lambda/iam.tf
+++ terraform/api/endpoint-hash/hash-lambda/iam.tf
@@ -14,19 +14,19 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# basic logs
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
-// For X-Ray permissions
+
 resource "aws_iam_role_policy_attachment" "lambda_xray" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
 }
 
-# policy
+
 resource "aws_iam_role_policy" "dynamodb_policy" {
   name = "${var.prefix}-dynamodb-policy"
   role = aws_iam_role.lambda_role.id
@@ -58,22 +58,23 @@ resource "aws_iam_role_policy" "kms_policy" {
   })
 }
 
-resource "aws_iam_role_policy" "ec2_policy" {
-  name = "${var.prefix}-ec2-network-interface-policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:CreateNetworkInterface",
-        "ec2:DeleteNetworkInterface",
-        "ec2:DescribeInstances",
-        "ec2:AttachNetworkInterface"
-      ]
-      Resource = "*"
-    }]
-  })
-}
+# resource "aws_iam_role_policy" "ec2_policy" {
+#   name = "${var.prefix}-ec2-network-interface-policy"
+#   role = aws_iam_role.lambda_role.id
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Action = [
+#         "ec2:DescribeNetworkInterfaces",
+#         "ec2:CreateNetworkInterface",
+#         "ec2:DescribeSubnets",
+#         "ec2:DeleteNetworkInterface",
+#         "ec2:AssignPrivateIpAddresses",
+#         "ec2:UnassignPrivateIpAddresses"
+#       ]
+#       Resource = "*"
+#     }]
+#   })
+# }
